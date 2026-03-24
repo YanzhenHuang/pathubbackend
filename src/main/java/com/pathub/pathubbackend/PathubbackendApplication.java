f384620830832b85efbf6f9672bc67efb27a1658
Yanzhen Huang <yanzhenhuangwork@gmail.com>
2026-03-24 19:20:40 +0800

    init: Init Repo

---
+++ src/main/java/com/pathub/pathubbackend/PathubbackendApplication.java
@@ -0,0 +1,13 @@
+package com.pathub.pathubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathubbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathubbackendApplication.class, args);
+	}
+
+}
